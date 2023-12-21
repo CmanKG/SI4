cb30f7dcc29caa4066b8f5dcae44a89838678255
CmanKG <143266959+CmanKG@users.noreply.github.com>
2023-12-21 14:54:03 +0530

    Add files via upload

---
+++ Supra.js
@@ -0,0 +1,45 @@
+status=""
+objects=[]
+function setup(){
+    canvas=createCanvas(800,500)
+    canvas.center()
+    }
+    function preload(){
+    img=loadImage("anthracite-supra-vented-hood.avif")
+    img=loadImage("anthracite-supra-vented-hood.avif")
+    object_detector=ml5.objectDetector("cocossd",modelloaded)
+    }
+    
+
+    function draw(){
+        image(img,0,0,800,500)
+        
+        if (status !="") {
+          for (i=0;i<objects.length;i++){
+            document.getElementById("status").innerHTML="Status:Object Detected"
+            fill("#FF0000");
+            percent=floor(objects[i].confidene*100);
+            text(objects[i].label+""+percent+"%",objects[i].x,objects[i].y);
+            noFill();
+            stroke("#FF0000")
+            rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height)
+        }
+        }
+        }
+        
+        
+    
+    function modelloaded(){
+      console.log("modelisloaded")  
+          status = true;  
+          object_detector.detect(img,gotResult)
+  }
+  function gotResult(error,results){
+    if (error) {
+      console.log(error)
+    } else {
+      console.log(results)
+      objects=results
+    }
+  }
+    
